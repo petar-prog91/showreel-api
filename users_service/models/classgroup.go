98a13f6ff64323df1aa934affdeab2837589906c
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2017-09-04 15:20:31 +0200

    Cleanup models

--- users_service/models/classgroup.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type ClassGroup struct {
-	ID                int    `db:"id, omitempty"`
-	ClassName         string `db:"classname, omitempty"`
-	ResponsiblePerson int    `db:"responsible_person, omitempty"`
-	CreationDate      string `db:"creation_date, omitempty"`
-}
--- users_service/models/student.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Student struct {
-	ID         int    `db:"id, omitempty"`
-	EnrollDate string `db:"enrolldate, omitempty"`
-	Parent     int    `db:"parent, omitempty"`
-	ClassGroup int    `db:"class_group, omitempty"`
-}
