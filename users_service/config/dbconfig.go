1397caf4d5d702efc52fdd1a7ab36b04835c5d03
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2017-10-21 19:51:23 +0200

    Fixing ports for MySql inside docker containers

--- users_service/config/dbconfig.go
+++ users_service/config/dbconfig.go
@@ -4,7 +4,7 @@ import "upper.io/db.v3/mysql"
 
 var DBSettings = mysql.ConnectionURL{
 	Database: `showreel`,
-	Host:     `showreel_mysql_db:3308`,
+	Host:     `showreel_mysql_db:3306`,
 	User:     `root`,
 	Password: `root`,
 }
