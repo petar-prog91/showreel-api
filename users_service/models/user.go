5e38b7a2f622cb89864d0ea50decde5ee7e22e20
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2017-10-21 20:11:41 +0200

    Changes to user model

--- users_service/models/user.go
+++ users_service/models/user.go
@@ -1,22 +1,9 @@
 package models
 
 type User struct {
-	Id          int    `db:"id, omitempty"`
-	Username    string `db:"username, omitempty"`
-	Password    string `db:"password, omitempty"`
-	SGroup      int    `db:"s_group, omitempty"`
-	FirstName   string `db:"first_name, omitempty"`
-	LastName    string `db:"last_name, omitempty"`
-	Address     string `db:"address, omitempty"`
-	City        string `db:"city, omitempty"`
-	ZipCode     int    `db:"zipcode, omitempty"`
-	Email       string `db:"email, omitempty"`
-	Phone       string `db:"phone, omitempty"`
-	DateOfBirth string `db:"date_of_birth, omitempty"`
-	Picture     string `db:"picture, omitempty"`
-}
-
-type UserRoles struct {
-	Id        int    `db:"id"`
-	Role_name string `db:"role_name"`
+	Id       int    `db:"id, omitempty"`
+	Username string `db:"username, omitempty"`
+	Password string `db:"password, omitempty"`
+	SGroup   int    `db:"s_group, omitempty"`
+	Email    string `db:"email, omitempty"`
 }
