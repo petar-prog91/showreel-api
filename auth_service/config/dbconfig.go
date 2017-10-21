b566dc2f0c2b3880a1bfb490aa9dc5a1db19f788
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2017-10-21 17:53:16 +0200

    Fixing database connections and docker images

--- auth_service/config/dbconfig.go
+++ auth_service/config/dbconfig.go
@@ -3,8 +3,8 @@ package config
 import "upper.io/db.v3/mysql"
 
 var DBSettings = mysql.ConnectionURL{
-	Database: `skolar_users`,
-	Host:     `users_service_mysql_db:3306`,
+	Database: `showreel`,
+	Host:     `showreel_mysql_db:3306`,
 	User:     `root`,
 	Password: `root`,
 }
