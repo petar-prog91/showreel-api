d0ee416b286835c705283cc6dbf040a074a8bbbc
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2017-10-22 18:44:45 +0200

    Hopefully this fixes it

--- users_service/models/user.go
+++ users_service/models/user.go
@@ -4,6 +4,6 @@ type User struct {
 	Id       int    `db:"id, omitempty"`
 	Username string `db:"username, omitempty"`
 	Password string `db:"password, omitempty"`
-	SGroup   int    `db:"sgroup, omitempty"`
+	Sgroup   int    `db:"sgroup, omitempty"`
 	Email    string `db:"email, omitempty"`
 }
